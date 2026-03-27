b26401a96211e08eb32a08cd8b999436abbc139c
Aashi <aashishah0475@gmail.com>
2026-03-27 21:30:15 +0530

    basics done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,12 @@
 export default function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
   return (
-    <footer>{new Date().toLocaleTimeString()} We're currently open!</footer>
+    <footer className="footer">
+      {new Date().toLocaleTimeString()}{" "}
+      {isOpen ? "We're currently open!" : "We're currently closed!"}
+    </footer>
   );
 }
--- src/components/Pizza.js
+++ src/components/Pizza.js
@@ -1,3 +1,12 @@
-export default function Pizza() {
-  return <div></div>;
+export default function Pizza(props) {
+  return (
+    <li className="pizza">
+      <img src={props.pizzaInfo.photoName} alt={props.pizzaInfo.name} />
+      <div>
+        <h3>{props.pizzaInfo.name}</h3>
+        <p>{props.pizzaInfo.ingredients}</p>
+        <span>${props.pizzaInfo.price}</span>
+      </div>
+    </li>
+  );
 }
